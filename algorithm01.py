9c413778d70db8e3849ec1f72d6d2c2a69a1da09
mooyeon-choi <dus1208@ajou.ac.kr>
2019-07-31 16:19:26 +0900

    190731 | algorithm - day1

--- algorithm01.py
+++ algorithm01.py
@@ -3,11 +3,11 @@ for tc in range(10):
     b_list = list(map(int, input().split()))
     result = 0
     for i in range(2, n-2):
-        max_value = 0
-        for j in range(-2, 3):
-            if j != 0:
-                if b_list[i+j] > max_value:
-                    max_value = b_list[i+j]
+        max_value = b_list[i-2]
+        for j in range(-1, 3):
+            if j == 0: continue
+            if b_list[i+j] > max_value:
+                max_value = b_list[i+j]
         if b_list[i] > max_value:
             result += b_list[i] - max_value
     print('#{} {}'.format(tc+1, result))
