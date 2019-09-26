5eac19ea5b055aba77e79268b5e7810872379298
mooyeon-choi <dus1208@ajou.ac.kr>
2019-09-26 09:21:14 +0900

    190925 | 알고리즘 풀이 추가

--- a형대비/sw_1949_등산로조성.py
+++ a형대비/sw_1949_등산로조성.py
@@ -8,14 +8,15 @@ def dfs(i, j, count, number):
                 visit[i + x[a]][j + y[a]] = 1
                 dfs(i+x[a], j+y[a], count + 1, mtn[i+x[a]][j+y[a]])
                 visit[i + x[a]][j + y[a]] = 0
-            elif not visit[i+x[a]][j+y[a]] and not used and mtn[i+x[a]][j+y[a]] < number + k:
+            elif not visit[i+x[a]][j+y[a]] and not used and mtn[i+x[a]][j+y[a]] - k < number:
                 visit[i + x[a]][j + y[a]] = 1
-                used = 1
+                used = True
                 dfs(i + x[a], j + y[a], count + 1, number - 1)
                 visit[i + x[a]][j + y[a]] = 0
-                used = 0
+                used = False
     else:
         return
+
 
 x = [-1, 1, 0, 0]
 y = [0, 0, -1, 1]
@@ -24,12 +25,12 @@ for tc in range(1, t + 1):
     answer = 0
     n, k = map(int, input().split())
     mtn = [list(map(int, input().split())) for _ in range(n)]
-    max_num = max(max(mtn))
+    max_num = max(map(max, mtn))
     for i in range(n):
         for j in range(n):
             if mtn[i][j] == max_num:
-                used = 0
+                used = False
                 visit = [[0]*n for _ in range(n)]
                 visit[i][j] = 1
                 dfs(i, j, 1, max_num)
-    print(answer)
+    print('#{} {}'.format(tc, answer))
