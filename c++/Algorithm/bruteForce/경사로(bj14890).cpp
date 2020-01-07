84672803afedb1afcd677a03b13a59f13eaf737d
mooyeon-choi <dus1208@ajou.ac.kr>
2020-01-08 00:38:23 +0900

    200108 | 원판돌리기 추가

--- c++/Algorithm/bruteForce/경사로(bj14890).cpp
+++ c++/Algorithm/bruteForce/경사로(bj14890).cpp
@@ -4,13 +4,15 @@ using namespace std;
 int N, L;
 int board[100][100];
 
+bool findRow(int n) {
+	for (int j = 0; j < N; j++) {
+
+	}
+}
+
 int main() {
 	cin >> N >> L;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) cin >> board[i][j];
-	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) cout << board[i][j];
-		cout << '\n';
 	}
 }
