e717f9c310036cac918d50f8a617001d79b016cb
mooyeon-choi <dus1208@ajou.ac.kr>
2020-01-09 23:29:00 +0900

    200109 | 알고리즘 풀이 추가

---
+++ python/ad_study/bj17779_게리맨더링2(미완).py
@@ -0,0 +1,21 @@
+def DFS(x, y, height, width):
+    if width < max_len:
+
+    else:
+        if height < max_len // 1 + 1 + (total_len - max_len):
+            if y - 1 > 0:
+
+
+
+N = int(input())
+board = [list(map(int, input().split())) for _ in range(N)]
+answer = 0xffffff
+region = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+
+for max_len in range(1, N + 1, 2):
+    for total_len in range(max_len, N + 1):
+        for i in range(N):
+            for j in range(max_len - 1, N - max_len + 1):
+                DFS(i, j, i, 1, total_len)
+
+print()
