826efdada81a91646dd82428054875811d08dc59
mooyeon-choi <dus1208@ajou.ac.kr>
2020-02-01 00:32:43 +0900

    200201 | Add 뿌요뿌요 풀이

--- python/ad_study/week_4/bj11559_PuyoPuyo.py
+++ python/ad_study/week_4/bj11559_PuyoPuyo.py
@@ -1,18 +1,20 @@
 def BFS(i, j, alpa):
-    nums = 1
     que = []
     que.append((i, j))
     idx = 0
     while idx < len(que):
         u, v = que[idx]
+        visit[u][v] = 1
         for k in range(4):
             if 0 <= u + dirs[k][0] < 12 and 0 <= v + dirs[k][1] < 6:
                 if board[u + dirs[k][0]][v + dirs[k][1]] == alpa:
-                    que.append((u + dirs[k][0], v + dirs[k][1]))
-                    nums += 1
-    if nums >= 4:
+                    if (u + dirs[k][0], v + dirs[k][1]) not in que:
+                        que.append((u + dirs[k][0], v + dirs[k][1]))
+        idx += 1
+    if len(que) >= 4:
         for k in range(len(que)):
             board[que[k][0]][que[k][1]] = '.'
+            cnt[que[k][1]] += 1
         return True
     else:
         return False
@@ -20,37 +22,43 @@ def BFS(i, j, alpa):
 
 def block_down():
     for i in range(6):
-        first, second = 11, 11
-        find = 0
-        while True:
-            if board[first][i] != '.':
-                first -= 1
-            else:
-                if second >= first or board[second][i] == '.':
-                    second -= 1
-                elif second < height[i]:
+        if height[i] < 12:
+            first, second = 11, 11
+            while True:
+                if first < 0:
                     break
+                if board[first][i] != '.':
+                    first -= 1
                 else:
-                    board[first][i], board[second][i] = board[second][i], board[first][i]
+                    if second < height[i]:
+                        break
+                    elif second >= first or board[second][i] == '.':
+                        second -= 1
+                    else:
+                        board[first][i], board[second][i] = board[second][i], board[first][i]
 
 
 answer = -1
 dirs = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 board = [list(input()) for _ in range(12)]
-height = [0]*6
+height = [12]*6
 for i in range(12):
     for j in range(6):
-        if not height[j] and board[i][j] != '.':
+        if height[j] == 12 and board[i][j] != '.':
             height[j] = i
 bomb = 1
 while bomb:
+    visit = [[0]*6 for _ in range(12)]
+    cnt = [0]*6
     bomb = 0
-    for i in range(max(height), 12):
+    for i in range(min(height), 12):
         for j in range(6):
-            if board[i][j] != '.':
+            if board[i][j] != '.' and not visit[i][j]:
                 if BFS(i, j, board[i][j]):
                     bomb = 1
     block_down()
+    for i in range(6):
+        height[i] += cnt[i]
     answer += 1
 
 print(answer)
